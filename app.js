fb5173ce1034ba7fa82babd4b9908410c01f426d
Cheah Chung Yin <cheahchungyin@gmail.com>
2019-07-05 17:21:53 +0800

    Adjust to testing branch

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let content = 
 `
-  <p>This is javascript text! Now supports multiple elements!</p>
+  <p>This is javascript testing branch!</p>
+  <p>This is a testing branch!</p>
 `
 document.getElementById("root").innerHTML = content;
