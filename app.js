42f80dc7f9ea0555a99da19fc3e8393949fe6c76
Cheah Chung Yin <cheahchungyin@gmail.com>
2019-07-05 17:26:08 +0800

    Add more content and style to p

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let content = 
 `
   <p>This is javascript text! Now supports multiple elements!</p>
+  <p>The main branch wants more content too!</p>
 `
 document.getElementById("root").innerHTML = content;
