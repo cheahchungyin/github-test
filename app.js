03b842f86bfdc42e9ff49c0dac8f0dad0261d2ab
Cheah Chung Yin <cheahchungyin@gmail.com>
2019-07-05 18:07:19 +0800

    New User

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 let content = 
 `
+  <h3>Ding ding! A new user entered the repo!</h3>
   <p>This is javascript text! Now supports multiple elements!</p>
   <p>The main branch wants more content too!</p>
 `
